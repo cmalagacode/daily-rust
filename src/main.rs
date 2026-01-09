9c6f327f5c3a0e01760de16babdb8e35f4faea01
Cristian Malaga <cmalaga720@gmail.com>
2026-01-09 18:33:27 -0500

    two pointer practice

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+fn two_pointer_a() {
+    let data = vec![1, 2, 3, 4, 5];
+    let mut left = 0;
+    let mut right = data.len() - 1;
+    while left < right {
+        println!("left: {} .. right: {}", left, right);
+        left += 1;
+        right -= 1;
+    }
+    println!("Data Is The Same: left: {} .. right: {}", left, right);
+}
+
+
 fn hello_world() {
     println!("Hello, world!");
 }
 
-
 fn main() {
     hello_world();
+    two_pointer_a();
 }
