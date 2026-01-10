fd89291e9be2dc73463f8bac4dcd9e6efb600ee6
Cristian Malaga <cmalaga720@gmail.com>
2026-01-10 15:45:47 -0500

    add slice and data types prac

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// String (String::from(&str)) vs &str (string slice)
+
+
 fn two_pointer_a() {
     let data = vec![1, 2, 3, 4, 5];
     let mut left = 0;
@@ -23,12 +26,53 @@ fn is_pali(x: &str) -> bool {
 }
 
 fn vect_test() {
-    let x = vec![1, 2, 3,4, 5];
+    let _x = vec![1, 2, 3,4, 5];
 }
 
 
 fn hello_world() {
     println!("Hello, world!");
+}
+
+fn shadowing_part2() {
+    let x = 10;
+    println!("x: {}", x);
+    let x = "Thi is a str";
+    println!("x: {}", x);
+    const _MAX_NUMBER: u32 = 100; // once created constants cannot be mutated .. must be known at compile time ..
+    static _MIN_NUMBER: u32 = 90; // const gets inlined .. but static is a constant that does not
+}
+
+fn usize_practice() {
+    let x: usize = 10;
+    println!("x: {}", x);
+    let b = true;
+    print!("{}\n", b);
+    let mut array_1: [i32; 5] = [4, 5, 6, 6, 10];
+    array_1[4] = 100;
+    println!("{:?}", array_1);
+    let array_2 = [0; 10];
+    println!("{:?}", array_2);
+}
+
+fn all_about_vector() {
+    let vec_1: Vec<i32> = vec![4, 5, 6, 8, 9];
+    println!("vec_1 values = {:?}", vec_1);
+}
+
+fn all_about_tuples() {
+    let database_row = ("122323", "John", "Doe", 40);
+    println!("database_row = {:?}", database_row);
+}
+
+fn fun_string() {
+    // string slice
+    let a = "this is a string slice";
+    let mut b = String::from(a); // this is a string that is mutable
+    let mut b_vec: Vec<char> = b.chars().collect();
+    b_vec[0] = '0';
+    b = b_vec.into_iter().collect();
+    println!("Printing mutaded string: {}", b);
 }
 
 fn main() {
@@ -38,6 +82,15 @@ fn main() {
     let result = is_pali("abcdcba");
     println!("Result is {} it is a pali", result);
     shadowing_rusty();
+    shadowing_part2();
+    usize_practice();
+    println!("==============");
+    all_about_vector();
+    println!("==============");
+    println!("==============");
+    all_about_tuples();
+    println!("==============");
+    fun_string();
 }
 fn shadowing_rusty() {
     let x = 5;
