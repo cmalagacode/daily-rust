6f5eb46f413cdf7edd690462676cfab0d4731ade
Cristian Malaga <cmalaga720@gmail.com>
2026-01-09 23:11:30 -0500

    shadowing added

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,22 @@ fn two_pointer_a() {
     }
     println!("Data Is The Same: left: {} .. right: {}", left, right);
 }
+fn is_pali(x: &str) -> bool {
+    let mut left = 0;
+    let mut right = x.len() - 1;
+    while left < right {
+        if x.chars().nth(left) != x.chars().nth(right) {
+            return false;
+        }
+        left += 1;
+        right -= 1;
+    }
+    return true;
+}
+
+fn vect_test() {
+    let x = vec![1, 2, 3,4, 5];
+}
 
 
 fn hello_world() {
@@ -18,4 +34,19 @@ fn hello_world() {
 fn main() {
     hello_world();
     two_pointer_a();
+    // abcdcba
+    let result = is_pali("abcdcba");
+    println!("Result is {} it is a pali", result);
+    shadowing_rusty();
+}
+fn shadowing_rusty() {
+    let x = 5;
+    println!("{}", x);
+    {
+        let x = 10;
+        println!("{}", x);
+    }
+
+    let x = 22; // shadow
+    println!("{}", x);
 }
